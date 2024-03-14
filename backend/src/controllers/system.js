a8319d14fcf693df8f35f564dc6069cd00674805
TrH203 <hoangtrhien203@gmail.com>
2024-03-14 18:28:45 +0700

    get all feature api

--- backend/src/controllers/system.js
+++ backend/src/controllers/system.js
@@ -5,10 +5,14 @@ const featureModel = require("../models/features.js");
 const has = require('has-keys');
 
 const getAllFeature = async (req, res) => {
-
+    const allFeature = await featureModel.findAll();
+    console.log(allFeature);
+    res.json(allFeature)
 }
 
 
 module.exports = {
     getAllFeature: getAllFeature,
 }
+
+
--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -4,6 +4,6 @@ const router = require('express').Router();
 // Users routes
 
 router.use(require('./user'));
-
+router.use(require("./system"))
 
 module.exports = router;
